002a4e3301d9d16e76a3da5bcd82b9dbe10d62c8
Simon Morvan <simon@icilalune.com>
2014-04-29 18:17:40 +0200

    Moving to trusty and tweak to stay on "lxc" engine.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,9 @@ sh -c "wget -qO- https://get.docker.io/gpg | apt-key add -"
 sh -c "echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
 
 apt-get update
-apt-get install linux-image-generic-lts-raring linux-headers-generic-lts-raring virtualbox-guest-utils lxc-docker graphviz tmux -y
+apt-get install virtualbox-guest-utils lxc lxc-docker graphviz tmux -y
+
+echo 'DOCKER_OPTS="-e lxc"' >> /etc/default/docker
 
 
 
