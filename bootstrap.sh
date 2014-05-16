47db63ab03d4d27a56529b9cf4d03aa615902758
Simon Morvan <simon@icilalune.com>
2014-05-16 10:31:38 +0200

    Install kernel extras to enable back AUFS support in docker

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,9 +4,9 @@ sh -c "wget -qO- https://get.docker.io/gpg | apt-key add -"
 sh -c "echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
 
 apt-get update
-apt-get install virtualbox-guest-utils lxc lxc-docker graphviz tmux -y
+apt-get install virtualbox-guest-utils lxc lxc-docker graphviz tmux linux-image-extra-`uname -r` -y
 
-echo 'DOCKER_OPTS="-e lxc"' >> /etc/default/docker
+echo 'DOCKER_OPTS="-e lxc -s aufs"' >> /etc/default/docker
 
 
 
