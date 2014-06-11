6f9fe743d13e4da2092ec31084ac88312a5d1081
Simon Morvan <simon@icilalune.com>
2014-06-11 17:18:51 +0200

    Add update & remove package specific to ubuntu

--- packer/docker.sh
+++ packer/docker.sh
@@ -2,9 +2,10 @@
 # Add docker.io APT repository
 sh -c "wget -qO- https://get.docker.io/gpg | apt-key add -"
 sh -c "echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
+apt-get update
 
 # Install docker & kernel additions
-apt-get install lxc lxc-docker graphviz tmux linux-image-extra-`uname -r` -y
+apt-get install lxc lxc-docker graphviz tmux -y
 
 # Alter docker default options to use LXC engine & AUFS storage
 echo 'DOCKER_OPTS="-e lxc -s aufs"' >> /etc/default/docker
