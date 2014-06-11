24e1e1b3d60099e750d484911ff0c0a6bd9116e7
Simon Morvan <simon@icilalune.com>
2014-06-11 12:52:50 +0200

    Remove docker configs

--- dockerfiles/graphite/start.sh
+++
@@ -1,5 +0,0 @@
-#/bin/sh
-
-/etc/init.d/apache2 start
-/opt/graphite/bin/carbon-cache.py start
-cd /opt/statsd ; node ./stats.js ./localConfig.js
--- dockerfiles/solr-drupal/foreground.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export CATALINA_HOME=/usr/share/tomcat6
-export CATALINA_BASE=/var/lib/tomcat6
-
-/usr/share/tomcat6/bin/catalina.sh run
